75f4a794a6f96c369c6393c2e02efe9d730016a3
Daniel Callus <danielcallus@gmail.com>
2021-05-13 12:35:30 +0100

    fixed bracket bug

--- src/components/TotalPopulation.js
+++ src/components/TotalPopulation.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 const getTotalPopulation = ({countries}) => {
     const TotalPopulation = countries.reduce((runningTotal, country) => runningTotal + country.population, 0);
-        return  TotalPopulation
     
     return (
         <>
-        <p>{TotalPopulation}</p>
+        <p>Total World Population: {TotalPopulation}</p>
         </>
     )
     
